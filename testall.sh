58662c94fe7608d664acea2c1a85d05712f47ddc
yucensun <kallla@umich.edu>
2021-04-25 12:02:15 -0400

    change tc

--- testall.sh
+++ testall.sh
@@ -6,13 +6,13 @@
 #  Compile and check the error of each expected-to-fail test
 
 # Path to the LLVM interpreter
-LLI="/usr/local/opt/llvm/bin/lli"
-# LLI="/usr/bin/lli"
+# LLI="/usr/local/opt/llvm/bin/lli"
+LLI="/usr/bin/lli"
 # LLI="lli"
 
 # Path to the LLVM compiler
-LLC="/usr/local/opt/llvm/bin/llc"
-# LLC="/usr/bin/llc"
+# LLC="/usr/local/opt/llvm/bin/llc"
+LLC="/usr/bin/llc"
 # LLC="llc"
 
 # Path to the C compiler
